c14747d4868775e553d8ff8880ead3347faa8626
Jakob Soukthavong <soukthavongjp@acad.sunybroome.edu>
2025-04-14 14:01:47 -0400

    Jakob Soukthavong Branch First Commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,40 @@
 //Project 2
 //Kristian Tokos and Jakob Soukthavong
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <set>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//Constants
+const int SCREEN_WIDTH = 80;
+const char SYMBOL = '*';
+
+//Prototypes
+void OutputDivider(char symbol, int width);
+
+
+int main(void)
+{
+
+
+	return 0;
+}
+
+/******************************************************************************
+Function #1
+OutputDivider: This function will print a divider line to the stream passed
+				into the function.
+******************************************************************************/
+void OutputDivider(char symbol, int width)
+{
+	cout << setfill(SYMBOL) << setw(SCREEN_WIDTH) << SYMBOL
+		<< setfill(' ') << endl;
+}
+
